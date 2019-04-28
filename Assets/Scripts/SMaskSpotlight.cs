61e4f3afb27fb149444814f8432f9e018388be32
j0bbee <joebinns.95@gmail.com>
2019-04-28 01:23:38 +0100

    #6

---
+++ Assets/Scripts/SMaskSpotlight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMaskSpotlight : MonoBehaviour
+{
+    [Range(0f, 2f)]
+    public float addSize;
+
+    float timer = 0;
+
+    float flickTime;
+
+    private void calculateBeatFreq()
+    {
+        float bpm = 120f;
+        flickTime = bpm / (60f * 4f);
+    }
+
+    private void Start()
+    {
+        calculateBeatFreq();
+    }
+
+    void Update()
+    {
+        timer += (Time.deltaTime);
+
+        addSize = Mathf.Sin(timer * Mathf.PI * flickTime) * 2;
+
+        //addSize = Mathf.Sin(timer * Mathf.PI / 2) * Mathf.Log10(timer * Mathf.PI / 2);
+
+        transform.localScale = new Vector3(addSize, addSize, 1);
+    }
+}
